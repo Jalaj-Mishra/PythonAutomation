609e8af4d0f5e69ded57f0742ce0a6e63b68b923
Jalaj-Mishra <mjalaj77@gmail.com>
2025-03-07 18:01:41 +0530

    Project started

---
+++ Selenium/project1.py
@@ -0,0 +1,12 @@
+
+# Project link - https://youtu.be/e9Bf769mXu0?si=-YkQ7Mr4NfouHAA-
+
+# Requirements:
+
+# Open http://www.tutorialsninja.com/demo 
+# Select 2 iphones
+# Take 1 screenshot
+# Select 1 laptop(HP LP3065)
+# Select delievery date: 31-12-2026
+# Create a Guest account and complete the checkout process
+# Print the total price and confirmation message in the console
